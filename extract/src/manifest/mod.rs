3312b150a6ef493bb54b796bee2e90cf75165b0d
fireyy <fireyy@gmail.com>
2022-10-10 13:54:41 +0800

    feat: add debug display for Manifest

--- extract/src/manifest/mod.rs
+++ extract/src/manifest/mod.rs
@@ -1,20 +1,19 @@
-use std::{
-    fs::File,
-};
-use crate::{
-    error::{ExtResult},
-};
+use core::fmt;
 
 mod ipa;
 mod apk;
 
 #[derive(Clone, Debug, Default)]
 pub struct Manifest {
-    name: String,
-    icon: String,
-    bundle_id: String,
-    version: String,
-    build_number: String,
+    pub name: String,
+    pub icon: String,
+    pub bundle_id: String,
+    pub version: String,
+    pub build_number: String,
+}
+
+impl fmt::Display for Manifest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { fmt::Debug::fmt(self, f) }
 }
 
 pub use ipa::{IPA_EXT, IpaManifest};
